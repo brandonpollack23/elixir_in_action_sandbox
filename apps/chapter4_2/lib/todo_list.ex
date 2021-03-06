b14494245fac06836d3fb43a9a1a946a3c14e2c3
Brandon Pollack <brandonpollack23@gmail.com>
2021-03-06 17:38:27 -0500

    update and TodoEntry type added

--- apps/chapter4_2/lib/todo_list.ex
+++ apps/chapter4_2/lib/todo_list.ex
@@ -1,7 +1,16 @@
+defmodule TodoList.TodoEntry do
+  defstruct id: -1, value: nil
+
+  def new(id, value), do: %__MODULE__{id: id, value: value}
+
+  def change_value(%__MODULE__{id: id}, value), do: %__MODULE__{id: id, value: value}
+end
+
 defmodule TodoList do
   @moduledoc """
   TodoList implementation from chapter 4 of EiA
   """
+  alias TodoList.TodoEntry, as: TodoEntry
 
   defstruct auto_id: 1, entries: Map.new()
 
@@ -14,10 +23,30 @@ defmodule TodoList do
   Adds a new entry to the given TodoList
   """
   def add_entry(%TodoList{entries: entries, auto_id: auto_id} = todo_list, entry) do
-    entry = Map.put(entry, :id, auto_id)
     new_entries = Map.put(entries, auto_id, entry)
 
     %TodoList{todo_list | entries: new_entries, auto_id: auto_id + 1}
+  end
+
+  def update_entry(%TodoList{} = todo_list, %TodoEntry{} = new_entry) do
+    update_entry(todo_list, new_entry.id, fn _ -> new_entry end)
+  end
+
+  def update_entry(%TodoList{} = todo_list, %TodoEntry{id: id}, updater_fun) do
+    update_entry(todo_list, id, updater_fun)
+  end
+
+  def update_entry(%TodoList{entries: entries} = todo_list, entry_id, updater_fun) do
+    case entries[entry_id] do
+      nil ->
+        todo_list
+
+      old_value ->
+        # Ensure the old entry is unchanged and run update function.
+        new_value = updater_fun.(old_value)
+        new_entries = Map.put(entries, entry_id, new_value)
+        %TodoList{todo_list | entries: new_entries}
+    end
   end
 
   @doc """
@@ -26,6 +55,6 @@ defmodule TodoList do
   def entries(%TodoList{entries: entries}, date) do
     entries
     |> Stream.filter(fn {_, entry} -> entry.date == date end)
-    |> Enum.map(fn {_, entry} -> entry end)
+    |> Enum.map(fn {id, entry} -> %TodoEntry{id: id, value: entry} end)
   end
 end
