43c26d980fcbce57c1160713639fd1e6726310f4
Brandon Pollack <brandonpollack23@gmail.com>
2021-03-12 17:46:57 -0500

    chapter 6

--- apps/tail_recur_test/test/tail_recur_test_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule TailRecurTestTest do
-  use ExUnit.Case
-  doctest TailRecurTest
-
-  test "greets the world" do
-    assert TailRecurTest.hello() == :world
-  end
-end
---
+++ lib/elixir_in_action.ex
@@ -0,0 +1,18 @@
+defmodule Chapter6 do
+  @moduledoc """
+  Documentation for `ElixirInAction`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ElixirInAction.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
